337be73ab6ec5bcf080ed0034d35988805a860c1
dimasmiftah <dimas.mfth@gmail.com>
2021-09-19 14:53:54 +0700

    change copy clipboard to show input

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const cardsElement = document.querySelector('.cards');
 
-const copyToClipboard = (text) => {
-  navigator.clipboard.writeText(text).then(
-    function () {
-      alert('Tautan telah disalin: ' + text);
-    },
-    function () {
-      alert('Gagal menyalin tautan');
-    }
-  );
+const showInput = (index) => {
+  const inputElement = document.querySelectorAll('.card__input')[index];
+  inputElement.hidden = inputElement.hidden ? false : true;
+  inputElement.select();
+  inputElement.setSelectionRange(0, inputElement.value.length);
 };
+
 fetch('./data/links.json')
   .then((response) => response.json())
   .then((data) => {
@@ -19,22 +16,10 @@ fetch('./data/links.json')
           <div class="card__header">
             <a href="${link?.url}" rel="external" class="card__header__title">${link?.title}</a>
             <button class="card__header__button" >
-              <svg
-                class="card__header__button__icon"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-                ></path>
-              </svg>
+              <svg class="card__header__button__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
             </button>
           </div>
+          <input class="card__input" type="text" name="link" id="link" value="${link?.url}" hidden>
           <p class="card__body">
             ${link?.body}
           </p>
@@ -42,15 +27,14 @@ fetch('./data/links.json')
       `;
       cardsElement.innerHTML += cardElement;
     });
-    return data?.links;
   })
-  .then((links) => {
+  .then(() => {
     const copyButtonElements = document.querySelectorAll(
       '.card__header__button'
     );
     copyButtonElements.forEach((button, index) =>
       button.addEventListener('click', () => {
-        copyToClipboard(links[index]?.url);
+        showInput(index);
       })
     );
   })
