678567ecd497199f356084b5ac69bc68d9946dd9
dimasmiftah <dimas.mfth@gmail.com>
2021-09-19 15:30:43 +0700

    change icon and smaller font size

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ fetch('./data/links.json')
           <div class="card__header">
             <a href="${link?.url}" rel="external" class="card__header__title">${link?.title}</a>
             <button class="card__header__button" >
-              <svg class="card__header__button__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
+              <svg class="card__header__button__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
             </button>
           </div>
           <input class="card__input" type="text" name="link" id="link" value="${link?.url}" hidden>
