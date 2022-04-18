67260cbc4bce6bbc55d5392fb7d364ccbec57c09
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2022-04-18 21:25:50 +0700

    feat: add action buttons to product card

--- app.js
+++ app.js
@@ -28,9 +28,19 @@ const addClickListener = () => {
 };
 
 const addCardComponent = (
-  { image = '', url, title, body, tag, published_date },
+  {
+    image = '',
+    shopee = '',
+    tokopedia = '',
+    url = '',
+    title,
+    body,
+    tag,
+    published_date,
+  },
   section
 ) => {
+  console.log(image, shopee, tokopedia, url, title);
   const isExpired = getExpiration(published_date);
   const cardElement = isExpired
     ? ''
@@ -41,16 +51,23 @@ const addCardComponent = (
               ? `<img
           src="${image}"
           class="card__image"
-        />`
+        /> <div class="card__actions">
+        <a href="${shopee}" target="_blank" rel="noopener" class="card__actions__button">Shopee</a>
+        <a href="${tokopedia}" target="_blank" rel="noopener" class="card__actions__button">Tokopedia</a>
+      </div>`
               : ''
           }
           <div class="card__header">
-            <a href="${url}" rel="external" class="card__header__title">${title}</a>
+            <a href="${url}" target="_blank"
+        rel="noopener noreferrer" class="card__header__title">${title}</a>
             <button class="card__header__button" >
               <svg class="card__header__button__icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
             </button>
           </div>
-          <input class="card__input" type="text" name="link" id="link" value="${url}" hidden>
+          <input class="card__input" type="text" name="link" id="link" value="${
+            url || shopee
+          }" hidden>
+
           <p class="card__body">
             ${body}
           </p>
